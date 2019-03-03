c32e13d41783c8c1b356016cf55d5a781eedc973
Austen Constable <austen@crossfire.ltd.uk>
2019-03-03 21:17:38 +0000

    Initial commit

---
+++ web/src/main/java/team/teamservice/web/team/TeamController.java
@@ -0,0 +1,12 @@
+package team.teamservice.web.team;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    private TeamRepository repository;
+
+}
---
+++ web/src/main/java/team/teamservice/web/team/TeamRepository.java
@@ -0,0 +1,13 @@
+package team.teamservice.web.team;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource(collectionResourceRel = "team", path = "team")
+public interface TeamRepository extends MongoRepository<Team, String>  {
+
+Optional<Team> findByTeamId(String teamId);
+
+}

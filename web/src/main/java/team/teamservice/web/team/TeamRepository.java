8a2dce9df2dfdc4f63413165f8c446a9afd24d09
Austen Constable <awconstable@gmail.com>
2021-03-31 10:41:24 +0200

    #22 Remove obsolete team migration code

--- web/src/main/java/team/teamservice/web/team/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package team.teamservice.web.team;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "team", path = "team")
-public interface TeamRepository extends MongoRepository<Team, String>  {
-
-}
